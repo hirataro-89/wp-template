03ad9c2e6ef37d882f5d00afd7bb3b768fa0c67b
yutaro hirabayashi <hirataro.yu@gmail.com>
2024-09-29 19:36:37 +0900

    リファクタ：ディレクトリ整理

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,16 +1,4 @@
 import '../style/style.scss?inline';
-import imgSrc from "/images/js.png";
-
-// jsから画像を読み込むサンプル
-const canvas = document.querySelector("#canvas");
-const context = canvas.getContext("2d");
-const image = new Image();
-image.width = 300;
-image.height = 300;
-image.src = imgSrc;
-image.onload = function() {
-  context.drawImage(image, 0, 0, 300, 300);
-};
 
 /* 360px以下はviewport固定 */
 !function() {
